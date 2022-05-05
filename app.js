b55ff166d6b89a5e0510e9618905971e6347dcef
Rafael Mendes Martins <rafael_mmartins@cm-loures.pt>
2022-05-05 11:02:33 +0100

    First Commit

---
+++ app.js
@@ -0,0 +1,44 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+
+let items = ["Trabalhar", "Treinar", "Tomar Banho"];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+
+  let today = new Date();
+
+  let options = {
+
+    day: "numeric",
+    weekday: "long",
+    month: "long",
+
+  };
+
+
+  let day = today.toLocaleDateString("pt-PT", options);
+
+res.render('list', {kindOfDay: day, newListItems: items});
+
+});
+
+app.post("/", function(req, res){
+  let item = req.body.newItem;
+
+items.push(item);
+
+  res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running in port 3000");
+});
