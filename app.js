20766431baac373cc20feb6467cb031beb50aa1b
Rafael Mendes Martins <rafael_mmartins@cm-loures.pt>
2022-05-06 11:08:36 +0100

    changing some content and other tools to improve it

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js")
+
 const app = express();
 
-
-let items = ["Work", "Gym Workout", "Take a bath"];
+const items = ["Work", "Gym Workout", "Take a bath"];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -14,29 +16,40 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res) {
 
-  let today = new Date();
+const day = date.getDate();
 
-  let options = {
-
-    day: "numeric",
-    weekday: "long",
-    month: "long",
-
-  };
-
-
-  let day = today.toLocaleDateString("pt-PT", options);
-
-res.render('list', {kindOfDay: day, newListItems: items});
+  res.render('list', {
+    listTitle: day,
+    newListItems: items
+  });
 
 });
 
-app.post("/", function(req, res){
+app.post("/", function(req, res) {
+
+  const item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
+});
+
+app.get("/work", function(req, res) {
+  res.render('list', {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
+});
+
+app.post("/work", function(req, res) {
   let item = req.body.newItem;
-
-items.push(item);
-
-  res.redirect("/");
+  workItems.push(item);
+  res.redirect("/work");
 })
 
 app.listen(process.env.PORT || 3000, function() {
